b096dfe0c1c9961a9591e99e68ca064c086d9d93
LordCodie <153275673+LordCodie@users.noreply.github.com>
2024-03-04 11:45:09 +0000

    got lost

--- src/app.js
+++ src/app.js
@@ -1,13 +1,45 @@
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+
 const express = require('express');
 const path = require('path');
+const JOBS = require('./jobs');
+const mustacheExpress = require('mustache-express');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'pages'));
+app.set('view engine', 'mustache');
+app.engine('mustache', mustacheExpress());
 
 app.get('/', (req, res) => {
     // res.send('Hello World!');
-    res.sendFile(path.join(__dirname, 'pages/index.html'));
+    // res.sendFile(path.join(__dirname, 'pages/index.html'));
+    res.render('index', { jobs: JOBS });
+});
+
+app.get('/jobs/:id', (req, res) => {
+    const id = req.params.id;
+    const matchedJob = JOBS.find(job => job.id.toString() === id);
+    res.render('job', { job: matchedJob });
+}) 
+
+app.post('/job/:id/apply', (req, res) => {
+    res.send('Got the application');
+})
+
+const transporter = nodemailer.createTransport({
+    host: 'mail.gmx.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_ID,
+      pass: process.env.EMAIL_PASSWORD
+    }
 });
 
 const port = process.env.PORT || 3000;
